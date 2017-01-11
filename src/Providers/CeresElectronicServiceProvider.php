e0145f06bc11518149fb36028199ffd76e0e33d4
Jan Mense <janmense@googlemail.com>
2017-01-11 14:22:10 +0100

    replaced singleitem view

--- src/Providers/CeresElectronicServiceProvider.php
+++ src/Providers/CeresElectronicServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace CeresElectronic\Providers;
 
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class CeresElectronicServiceProvider extends ServiceProvider
 {
@@ -12,4 +15,11 @@ class CeresElectronicServiceProvider extends ServiceProvider
      */
 
     public function register() {}
+
+    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    {
+    	$eventDispatcher->listen('tpl.item', function(TemplateContainer $container, $templateData) {
+			$container->setTemplate("CeresElectronic::CeresElectronicSingleItem.twig");
+		}, -1);
+    }
 }

9a90682aa022022e2c77da9aa6cde4d6203a5e83
Jan Mense <janmense@googlemail.com>
2017-01-26 17:35:46 +0100

    added homepage tpl for hipster

--- src/Providers/CeresHipsterServiceProvider.php
+++ src/Providers/CeresHipsterServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace CeresHipster\Providers;
 
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class CeresHipsterServiceProvider extends ServiceProvider
 {
@@ -11,5 +14,16 @@ class CeresHipsterServiceProvider extends ServiceProvider
      * Register the service provider.
      */
 
-    public function register() {}
+    public function register() {
+         
+    }
+
+    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    {
+
+        // provide template to use for homepage
+        $eventDispatcher->listen('tpl.home', function(TemplateContainer $container, $templateData) {
+            $container->setTemplate("CeresHipster::Homepage.Homepage");
+        }, 99);
+    }
 }

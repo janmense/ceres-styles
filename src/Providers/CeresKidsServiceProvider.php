b9bffebe1a1ec0ea83a8845a46f2f2f80e9a567e
Jan Mense <janmense@googlemail.com>
2017-01-27 16:25:14 +0100

    updated kids styles

--- src/Providers/CeresKidsServiceProvider.php
+++ src/Providers/CeresKidsServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace CeresKids\Providers;
 
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class CeresKidsServiceProvider extends ServiceProvider
 {
@@ -11,5 +14,16 @@ class CeresKidsServiceProvider extends ServiceProvider
      * Register the service provider.
      */
 
-    public function register() {}
+    public function register() {
+         
+    }
+
+    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    {
+
+        // provide template to use for homepage
+        $eventDispatcher->listen('tpl.home', function(TemplateContainer $container, $templateData) {
+            $container->setTemplate("CeresKids::Homepage.Homepage");
+        }, 99);
+    }
 }

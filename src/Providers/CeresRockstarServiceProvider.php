ae072475ee58378191daa0e16b97f07ab510eacb
Jan Mense <janmense@googlemail.com>
2017-02-22 16:48:31 +0100

    updated rockstar theme

--- src/Providers/CeresRockstarServiceProvider.php
+++ src/Providers/CeresRockstarServiceProvider.php
@@ -1,17 +1,26 @@
 <?php
- 
+
 namespace CeresRockstar\Providers;
- 
+
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
- 
+use Plenty\Plugin\Templates\Twig;
+
 class CeresRockstarServiceProvider extends ServiceProvider
 {
- 
-	/**
-	 * Register the service provider.
-	 */
-	public function register()
-	{
- 
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register() {
+         
+    }
+    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    {
+        // provide template to use for homepage
+        $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
+            $container->setTemplate("CeresRockstar::Homepage.Homepage");
+            return false;
+        });
+    }
 }

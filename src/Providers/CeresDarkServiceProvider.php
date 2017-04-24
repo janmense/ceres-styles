15620e82b4ef888ee6dd56a24f82da4ceb69e294
Jan Mense <janmense@googlemail.com>
2017-04-24 11:27:56 +0200

    update meta

--- src/Providers/CeresDarkServiceProvider.php
+++ src/Providers/CeresDarkServiceProvider.php
@@ -2,14 +2,20 @@
 
 namespace CeresDark\Providers;
 
+use IO\Extensions\Functions\Partial;
+use IO\Helper\CategoryKey;
+use IO\Helper\CategoryMap;
 use IO\Helper\TemplateContainer;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ConfigRepository;
+use IO\Helper\ComponentContainer;
+
 
 class CeresDarkServiceProvider extends ServiceProvider
 {
-
+    const EVENT_LISTENER_PRIORITY = 99;
     /**
      * Register the service provider.
      */
@@ -18,7 +24,7 @@ class CeresDarkServiceProvider extends ServiceProvider
          
     }
 
-    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
     {
 
         // provide template to use for homepage

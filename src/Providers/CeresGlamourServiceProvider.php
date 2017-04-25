db8a1817086a430449fa9d6cd4a315638c2c2cb4
Jan Mense <janmense@googlemail.com>
2017-04-25 14:44:59 +0200

    update styles for glamour

--- src/Providers/CeresGlamourServiceProvider.php
+++ src/Providers/CeresGlamourServiceProvider.php
@@ -31,5 +31,11 @@ class CeresGlamourServiceProvider extends ServiceProvider
             $container->setTemplate("CeresGlamour::Homepage.Homepage");
             return false;
         });
+
+        // provide template to use for homepage
+        $eventDispatcher->listen('IO.tpl.category.item', function(TemplateContainer $container, $templateData) {
+            $container->setTemplate("CeresGlamour::Category.Item.CategoryItem");
+            return false;
+        });
     }
 }

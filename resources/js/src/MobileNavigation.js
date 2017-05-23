10dd5b8dd746b16633aa0b559f48a677fcdf124d
Jan Mense <janmense@googlemail.com>
2017-05-23 13:50:37 +0200

    Revert "some fixes"
    This reverts commit 773df8dc81df23e580f6e342d3e852394509ac56.

--- resources/js/src/MobileNavigation.js
+++ resources/js/src/MobileNavigation.js
@@ -1,89 +1,86 @@
-(function()
+var MobileNavigation = new Vue({
+    el: "#MobileNavigation",
+    data: {
+        items: [],
+        activeItem: null
+    },
+    methods: {
+        getItemById: function(id)
 {
-    var MobileNavigation = new Vue({
-        el: "#MobileNavigation",
-        data: {
-            items: [],
-            activeItem: null
-        },
-        methods: {
-            getItemById: function(id)
+            for (var key in this.items)
 {
-                for (var key in this.items)
+                if (this.items[key].id === id)
 {
-                    if (this.items[key].id === id)
-{
-                        return this.items[key];
-                    }
+                    return this.items[key];
                 }
-                return null;
-            },
-            itemHasChildren: function({id: id})
-{
-                var items = this.getItemsByParentId(id);
-
-                return items.length > 0;
-            },
-            getItemsByParentId: function(id)
-{
-                return this.items.filter(function(item)
-{
-                    return item.parent === id;
-                });
-            },
-            triggerItem: function(item, useUrl)
-{
-                if (useUrl && item.url)
-{
-                    window.location.href = item.url;
-                }
-                else if (this.itemHasChildren(item))
-{
-                    this.setActiveItem(item.id);
-                }
-            },
-            getBreadcrumbItems: function(itemId)
-{
-                var items = [];
-                var tmp = this.getItemById(itemId);
-
-                while (tmp)
-{
-                    items.unshift(tmp);
-                    tmp = this.getItemById(tmp.parent);
-                }
-                return items;
-            },
-            addItem: function(id, title, parent, url)
-{
-                this.items.push({
-                    id: id,
-                    title: title,
-                    parent: parent,
-                    url: url
-                });
-            },
-            setActiveItem: function(id)
-{
-                this.activeItem = id;
-            },
-            doNothing: function()
-{
-                // console.log('doing nothing');
-            },
-
-            showMobileNavigation: function()
-{
-                $("#MobileNavigation").fadeIn();
-            },
-            hideMobileNavigation: function()
-{
-                $("#MobileNavigation").fadeOut();
             }
-        }
-    });
+            return null;
+        },
+        itemHasChildren: function({id: id})
+{
+            var items = this.getItemsByParentId(id);
 
-    window.document.MobileNavigation = MobileNavigation;
-    // lint requires us to do something with MobileNavigation. of course this is not the best fix :P
-    MobileNavigation.doNothing();
-})();
+            return items.length > 0;
+        },
+        getItemsByParentId: function(id)
+{
+            return this.items.filter(function(item)
+{
+                return item.parent === id;
+            });
+        },
+        triggerItem: function(item, useUrl)
+{
+            if (useUrl && item.url)
+{
+                window.location.href = item.url;
+            }
+            else if (this.itemHasChildren(item))
+{
+                this.setActiveItem(item.id);
+            }
+        },
+        getBreadcrumbItems: function(itemId)
+{
+            var items = [];
+            var tmp = this.getItemById(itemId);
+
+            while (tmp)
+{
+                items.unshift(tmp);
+                tmp = this.getItemById(tmp.parent);
+            }
+            return items;
+        },
+        addItem: function(id, title, parent, url)
+{
+            this.items.push({
+                id: id,
+                title: title,
+                parent: parent,
+                url: url
+            });
+        },
+        setActiveItem: function(id)
+{
+            this.activeItem = id;
+        },
+        doNothing: function()
+{
+            // console.log('doing nothing');
+        }
+    }
+});
+
+// lint requires us to do something with MobileNavigation. of course this is not the best fix :P
+MobileNavigation.doNothing();
+
+// these functions need to be global, because they may be called outside of the vue context
+/*eslint-disable */
+function showMobileNavigation() {
+    $('#MobileNavigation').fadeIn();
+}
+function hideMobileNavigation() {
+    $('#MobileNavigation').fadeOut();
+}
+/*eslint-enable */
